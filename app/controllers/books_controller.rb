0615b2411e2e4877013c8d7679bc51ef7f5442c0
rxgvnd <suryaniira11@gmail.com>
2021-08-09 16:02:05 +0700

    Finished

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :download]
+  
+    def index
+      @books = Book.order('created_at DESC')
+    end
+  
+    def new
+      @book = Book.new
+    end
+  
+    def show
+    end
+  
+    def create
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to books_path
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def book_params
+      params.require(:book).permit(:title, :description, :image)
+    end
+  
+    def set_book
+      @book = Book.find(params[:id])
+    end
+  end
